bb79750d3d6a6611409c0abc85610287485ef6c9
Haldhardwivedi <haldhardwivedi@gmail.com>
2021-07-13 17:42:24 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,14 @@
 import sys
 from  pytube import YouTube
 
+SAVE_PATH ="/home/"
+
 vid_link=str(sys.argv[1])
 yt =YouTube(vid_link)
 video = yt.streams.filter(only_audio=True).first()
-video.download()
+try:
+    video.download(SAVE_PATH) 
+except: 
+    print("Some Error!") 
 #ytmd.Downloader(sys.argv[1]);
 #print(sys.argv[1]); 
