7b994a8355fa4b1d54cb612b4337e0a8c4c28f68
Haldhardwivedi <haldhardwivedi@gmail.com>
2021-07-13 18:33:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,9 +32,8 @@ app.post('/', (req, res) => {
       if (err) throw err;
       // results is an array consisting of messages collected during execution
       console.log('results: %j', results);
-      res.download(__dirname+videolink);
+      res.sendFile(__dirname +"/downloaded.html");
     });
-    res.sendFile(__dirname +"/downloaded.html");
 })
 app.listen(port, () => console.log(`Example app listening on port 
 ${port}!`))
