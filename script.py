fa16d5eaed2533e20ce02b9324f8692aaff3bce9
haldhar dwivedi <haldhardwivedi@gmail.com>
2021-07-13 14:34:33 +0530

    adding files

---
+++ script.py
@@ -0,0 +1,11 @@
+import sys
+from pytube import YouTube
+sys.stdout.flush()
+
+
+vid_link=str(sys.argv[1])
+yt =YouTube(vid_link)
+video = yt.streams.filter(only_audio=True).first()
+video.download()
+#ytmd.Downloader(sys.argv[1]);
+#print(sys.argv[1]); 
