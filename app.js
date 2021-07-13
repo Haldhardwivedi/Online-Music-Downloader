763f542671251f901613ede23cfd260cdad7a12b
Haldhardwivedi <haldhardwivedi@gmail.com>
2021-07-13 16:08:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var  {PythonShell} = require('python-shell');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(express.static(path.join(__dirname, 'public/css')));
 
