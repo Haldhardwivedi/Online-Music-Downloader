277b3d5a56d372a2b1f1201060b4584c7f5b28b0
Haldhardwivedi <haldhardwivedi@gmail.com>
2021-07-14 16:20:20 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,13 @@
 import sys
 from  pytube import YouTube
-
+import os
 
 try:
     vid_link=str(sys.argv[1])
     yt =YouTube(vid_link)
     video = yt.streams.filter(only_audio=True).first()
-    video.download() 
+    out_file=video.download()
+    os.rename(out_file,"new.mp4") 
 except: 
     print("Some Error!") 
 #ytmd.Downloader(sys.argv[1]);
