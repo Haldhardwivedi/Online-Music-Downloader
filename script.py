2cbdd9fb7a5637d8ce6e6abcffea62f060a96f05
Haldhardwivedi <haldhardwivedi@gmail.com>
2021-07-14 23:10:17 +0530

    Update script.py

--- script.py
+++ script.py
@@ -7,9 +7,7 @@ try:
     yt =YouTube(vid_link)
     video = yt.streams.filter(only_audio=True).first()
     out_file=video.download()
-    prev = os.path.splitext(out_file)[0]
-    os.rename(out_file, prev + '.mp3')
-    print(prev + '.mp3')
+    print(out_file')
     #os.rename(out_file,"new.mp4") 
 except: 
     print("Some Error!") 
