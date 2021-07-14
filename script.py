1e0a1c028c7b7ddf77b70bb4419933d0bbba2181
Haldhardwivedi <haldhardwivedi@gmail.com>
2021-07-14 17:08:31 +0530

    Update script.py

--- script.py
+++ script.py
@@ -7,7 +7,8 @@ try:
     yt =YouTube(vid_link)
     video = yt.streams.filter(only_audio=True).first()
     out_file=video.download()
-    os.rename(out_file,"new.mp4") 
+    print(out_file)
+    #os.rename(out_file,"new.mp4") 
 except: 
     print("Some Error!") 
 #ytmd.Downloader(sys.argv[1]);
